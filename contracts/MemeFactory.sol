d5b88f186cd3cced0f5bdba0409d69649ce102a1
David Chukwudi Onyeka (Nokia) <david.chukwudi_onyeka@nokia.com>
2025-01-18 15:14:10 +0100

    meme token

---
+++ contracts/MemeFactory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MemeFactory {}
